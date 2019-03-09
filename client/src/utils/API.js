05bcda0cd866268e7d320fc60015d1e6f3e4fa30
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-09 13:30:01 -0800

    added AJAX call for individual plants by id

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,6 @@ const PLANTURL = "https://trefle.io/";
 const PLANTAPIKEY = "token=WGEzWlEvMDFpOGlCQ3haODB1MC81UT09";
 
 export default {
-  searchPlants: query => axios.get(PLANTURL + "api/plants?" + PLANTAPIKEY + query.map(param => "&" + param))
+  searchPlants: query => axios.get(PLANTURL + "api/plants?" + PLANTAPIKEY + query.map(param => "&" + param)),
+  searchPlant: id => axios.get(PLANTURL + "api/plants/" + id + "?" + PLANTAPIKEY)
 }
