27f28fcad42e327b0f16708b8abe712ce9447b84
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-09 13:21:30 -0800

    added API call for Trefle api/plants

---
+++ client/src/utils/API.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+const PLANTURL = "https://trefle.io/";
+const PLANTAPIKEY = "token=WGEzWlEvMDFpOGlCQ3haODB1MC81UT09";
+
+export default {
+  searchPlants: query => axios.get(PLANTURL + "api/plants?" + PLANTAPIKEY + query.map(param => "&" + param))
+}
