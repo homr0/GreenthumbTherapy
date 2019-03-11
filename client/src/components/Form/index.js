63a40a6ae5679ff8b63538039ce9aea33187652a
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-11 10:34:43 -0700

    updated Form inputs

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -2,8 +2,31 @@ import React from "react";
 
 export const Input = props => {
   return (
-    <div className="form-group">
-      <input className="form-control" {...props} />
+    <div className={"input-field col " + ((props.size) ? props.size : "s12")}>
+      <input className="validate" id={props.id} type={(props.type) ? props.type : "text" } />
+      <label for={props.id}>{props.children}</label>
     </div>
   );
 };
+
+export const Radio = props => {
+  return (
+    <p>
+      <label>
+        <input type="radio" name={props.name} value={props.value} />
+        <span>{props.children}</span>
+      </label>
+    </p>
+  );
+}
+
+export const Checkbox = props => {
+  return (
+    <p>
+      <label>
+        <input type="checkbox" id={props.id} className="filled-in" />
+        <span>{props.children}</span>
+      </label>
+    </p>
+  );
+}
