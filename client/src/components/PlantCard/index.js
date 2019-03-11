76d71c5a9841c964324d9739a9e26a2a639dadd1
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-11 10:09:41 -0700

    updated Plant Card to include updated List component

--- client/src/components/PlantCard/index.js
+++ client/src/components/PlantCard/index.js
@@ -1,18 +1,20 @@
 import React from "react";
+import {List, ListItem} from "../List";
 import "./style.css";
 
 const PlantCard = props => {
   return (
-    <div className="card">
-      <div className="img-container">
+    <div className={"card" + props.colors}>
+      <div className="card-image">
         <img alt={props.name} src={props.image} />
       </div>
-      <div className="content">
-        <ul>
-          <li>
+
+      <div className="card-content">
+        <List>
+          <ListItem>
             <strong>Name:</strong> {props.name}
-          </li>
-        </ul>
+          </ListItem>
+        </List>
       </div>
     </div>
   );
