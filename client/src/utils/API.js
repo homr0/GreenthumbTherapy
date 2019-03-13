554d370f0ab56760c61a4bfb9839c849afae79e4
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-13 11:49:07 -0700

    updated Questionnaire to return some responses from the API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,14 @@
 import axios from "axios";
+const PROXYURL = "https://cors-anywhere.herokuapp.com/"
 const PLANTURL = "https://trefle.io/";
-const PLANTAPIKEY = "token=WGEzWlEvMDFpOGlCQ3haODB1MC81UT09";
+const PLANTAPIKEY = "WGEzWlEvMDFpOGlCQ3haODB1MC81UT09";
 
 export default {
   // Goes to Trefle to find a selection of plants and individual plant information.
-  searchPlants: query => axios.get(PLANTURL + "api/plants?" + PLANTAPIKEY + query),
+  searchPlants: query => {
+    query.token = PLANTAPIKEY;
+    return axios.get(PROXYURL + PLANTURL + "api/plants", {params: query});
+  },
   searchPlant: id => axios.get(PLANTURL + "api/plants/" + id + "?" + PLANTAPIKEY),
 
   homeTest: () => axios.get("/api/home"),
