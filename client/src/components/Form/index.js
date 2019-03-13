71477579b731d91141d29b9aeee8f90f6c2508af
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-13 14:03:34 -0700

    added name property to Input

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Input = props => {
   return (
     <div className={"input-field col " + ((props.size) ? props.size : "s12")}>
-      <input className="validate" id={props.id} type={(props.type) ? props.type : "text" } onChange={props.handleInputChange} />
+      <input className="validate" id={props.id} type={(props.type) ? props.type : "text" } name={props.name} onChange={props.handleInputChange} />
       <label for={props.id}>{props.children}</label>
     </div>
   );
