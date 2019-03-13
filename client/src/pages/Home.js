066ed26149708deabaf2f3b47dcbc3f5c8f2c945
diggsn <42653979+DiggsNG@users.noreply.github.com>
2019-03-12 19:43:01 -0700

    updated home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,4 +5,30 @@ import Header from "../components/Header";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
+import { cpus } from "os";
 
+class Home extends Component {
+    state = {
+        plant: []
+    };
+
+render() {
+    return (
+       <container fluid>
+           <Row>
+               <Col size="s12 m4">
+               <h5>Plant Matcher</h5>
+               </Col>
+              <Col size="s12 m4">
+               <h5>Search Plants</h5>
+               </Col>
+               <Col size="s12 m4">
+               <h5>User Page</h5>
+              </Col>
+         </Row>
+       </container>
+    )
+}
+}
+
+export default Home;
