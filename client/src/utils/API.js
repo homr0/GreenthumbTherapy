7e8db976af6a18c39ef69677eb92cbfe3b11661f
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-16 23:09:19 -0700

    updated name for plant adding API call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,14 +5,10 @@ const PLANTAPIKEY = "WGEzWlEvMDFpOGlCQ3haODB1MC81UT09";
 
 export default {
   // Goes to Trefle to find a selection of plants and individual plant information.
-  searchPlants: query => {
-    // query.token = PLANTAPIKEY;
-    // return axios.get(PROXYURL + PLANTURL + "api/plants", {params: query});
-    return axios.post("/api/plants/search", query);
-  },
+  searchPlants: query => axios.post("/api/plants/search", query),
   searchPlant: id => axios.get(PROXYURL + PLANTURL + "api/plants/" + id + "?" + PLANTAPIKEY),
 
-  plantAddTest: query => axios.post("/api/plants/", query),
+  plantAdd: query => axios.post("/api/plants/", query),
 
   homeTest: () => axios.get("/api/home"),
   loginTest: userData => axios.post("/api/authenticate", userData),
