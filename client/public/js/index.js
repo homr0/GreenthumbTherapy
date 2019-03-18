37f743fd4f7f2cc20d3a92b4434a0035ad4b5644
wonder woman <rdelquiam@gmail.com>
2019-03-18 12:17:09 -0700

    added mobile menu

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // Initializes Materialize JavaScript components.
   var parallax = M.Parallax.init(document.querySelectorAll('.parallax'));
   var select = M.FormSelect.init(document.querySelectorAll('select'));
+  var instances = M.Sidenav.init(document.querySelectorAll('.sidenav'));
 });
+
+
+
