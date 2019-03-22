853184c92d605c53063333ea141e3d12f53e37f4
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-21 19:37:30 -0700

    added first and last name to User model

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {Btn}  from "../components/Btn";
 import API from "../utils/API";
 import { Input } from "../components/Form";
-import { Col, Container } from "../components/Grid";
+import { Col, Row, Container } from "../components/Grid";
 import Tabs from "../components/Tabs"
 
 export default class Register extends Component {
@@ -13,12 +13,16 @@ export default class Register extends Component {
     this.state = {
       register_email: "",
       register_password: "",
+      register_first_name: "",
+      register_last_name: "",
       register_message: "Please fill in the information to create your account.",
       register_status: 200,
+
       login_email: "",
       login_password: "",
       login_message: "Please type in the email and password for your account.",
       login_status: 200,
+
       activeTab: props.activeTab
     };
   };
@@ -37,14 +41,18 @@ export default class Register extends Component {
   handleFormSubmit = e => {
     e.preventDefault();
   
-    API.registerTest({
+    API.register({
       email: this.state.register_email,
-      password: this.state.register_password
+      password: this.state.register_password,
+      first_name: this.state.register_first_name,
+      last_name: this.state.register_last_name
     })
-    .then(res => this.setState({
-      register_status: res.data.status,
+    .then(res => {
+      console.log(res);
+      this.setState({
+      register_status: res.status,
       register_message: res.data.message
-    }))
+    })})
     .catch(err => this.setState({
       register_status: 500,
       register_message: "Internal server error!"
@@ -54,13 +62,13 @@ export default class Register extends Component {
   handleLogin = e => {
     e.preventDefault();
 
-    API.loginTest({
+    API.login({
       email: this.state.login_email,
       password: this.state.login_password,
     })
     .then(res => this.setState({
       login_message: res.data.message,
-      login_status: res.data.status
+      login_status: res.status
     }))
     .catch(err => this.setState({
         status: 500,
@@ -78,25 +86,43 @@ export default class Register extends Component {
         ]} activeTab={this.state.activeTab}>
           <Col id="register">
             <form>
-              <Input
-                size="s6"
-                name="register_email"
-                type="email"
-                id="register_email"
-                handleInputChange={this.handleInputChange}
-                >Email</Input>
-
+              <Row>
                 <Input
                   size="s6"
-                  name="register_password"
-                  type="password"
-                  id="register_password"
+                  name="register_first_name"
+                  id="register_first_name"
                   handleInputChange={this.handleInputChange}
-                  >Password</Input>
+                  >First Name</Input>
 
-                  <Btn handleClickEvent={this.handleFormSubmit}>Register User</Btn>
+                  <Input
+                    size="s6"
+                    name="register_last_name"
+                    id="register_last_name"
+                    handleInputChange={this.handleInputChange}
+                    >Last Name</Input>
+              </Row>
 
-                  <p className={(this.state.register_status === 200) ? "green-text" : "red-text"}>{this.state.register_message}</p>
+              <Row>
+                <Input
+                  size="s6"
+                  name="register_email"
+                  type="email"
+                  id="register_email"
+                  handleInputChange={this.handleInputChange}
+                  >Email</Input>
+
+                  <Input
+                    size="s6"
+                    name="register_password"
+                    type="password"
+                    id="register_password"
+                    handleInputChange={this.handleInputChange}
+                    >Password</Input>
+              </Row>
+
+              <Btn handleClickEvent={this.handleFormSubmit}>Register User</Btn>
+
+              <p className={(this.state.register_status === 200) ? "green-text" : "red-text"}>{this.state.register_message}</p>
             </form>
           </Col>
 
