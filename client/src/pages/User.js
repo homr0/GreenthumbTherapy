d41f653fc7e5d8f8a6b51a0e11776793cb594140
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-24 21:17:47 -0700

    updated plant lists to be in Tabs and moved other information into a profile card

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -4,6 +4,8 @@ import {Btn}  from "../components/Btn";
 import API from "../utils/API";
 import { Col, Row, Container } from "../components/Grid";
 import { Input } from "../components/Form";
+import Tabs from "../components/Tabs";
+import {List, ListItem} from "../components/List";
 import PlantCard from "../components/PlantCard";
 import profile from "../components/PlantCard/placeholder.png"
 
@@ -14,6 +16,7 @@ class User extends Component {
     this.state = {
       user: null,
       first_name: null,
+      last_name: "User",
       favorites: [],
       banned: [],
 
@@ -21,7 +24,8 @@ class User extends Component {
       plant_pets: [],
 
       email: null,
-      password: null
+      password: null,
+      password_status: ""
     };
   };
 
@@ -95,22 +99,57 @@ class User extends Component {
       password: this.state.password,
       new_password: this.state.new_password
     })
-      .then(res => console.log(res))
+      .then(res => this.setState({
+        password_status: res.data.message
+      }))
       .catch(err => console.log(err));
   }
 
   render() {
     return (
       <Container>
+        <Row>
+          <Col>
+            <h2 className="center">{this.state.first_name} {this.state.last_name}'s Planter</h2>
+          </Col>
+        </Row>
+
         <Row id="profile">
-          <Col size="s6">
-            <h2>{this.state.first_name} {this.state.last_name}</h2>
+          <Col size="s12 m3 offset-m1">
+            <img className="profile-img" src={(this.state.favorites.length > 0) ? this.state.favorites[0].image : profile} alt="Plant profile"/>
+          </Col>
+
+          <Col size="s12 profile-card m8">
+            <List>
+              <ListItem>
+                <strong>Primary Plant Locations: </strong>{(this.state.plant_location.length > 0) ? this.state.plant_location.join(", ") : "N/A"}
+              </ListItem>
+
+              <ListItem>
+                <strong>Average Sunlight: </strong>{((this.state.plant_light === "Intolerant") && <span><i className="fas fa-sun"></i> Full Sun</span>)
+                  || ((this.state.plant_light === "Intermediate") && <span><i className="fas fa-cloud-sun"></i> Partial Sun</span>)
+                  || ((this.state.plant_light === "Tolerant") && <span><i className="fas fa-cloud"></i> Shade</span>)
+                  || "N/A"}
+              </ListItem>
+
+              <ListItem>
+                <strong>Average Watering Frequency: </strong>{(this.state.plant_water !== "none") ? this.state.plant_water : "N/A"}
+              </ListItem>
+
+              <ListItem>
+                <strong>Pets to Consider: </strong>{(this.state.plant_pets.length > 0) ? this.state.plant_pets.join(", ") : "None"}
+              </ListItem>
+
+              <ListItem>
+                <strong>Allergies?: </strong>{this.state.plant_allergy ? "Yes" : "No"}
+              </ListItem>
+            </List>
             
             <form>
               <Input
                 name="password" type="password" id="password"
                 handleInputChange={this.handleInputChange}
-                size="s12">Old Password
+                size="s12">Current Password
               </Input>
 
               <Input
@@ -120,59 +159,43 @@ class User extends Component {
               </Input>
               <Btn handleClickEvent={this.handleFormSubmit}>Update Password</Btn>
             </form>
-          </Col>
 
-          <Col size="s4 offset-s2">
-            <img className="profile-img" src={(this.state.favorites.length > 0) ? this.state.favorites[0].image : profile} alt="Plant profile"/>
+            <p>{this.state.password_status}</p>
           </Col>
         </Row>
 
-        <Row>
-          <Col size="s12 m4">
-            <h3 className="center">Favorites</h3>
-
+        <Tabs tabs={[
+          {link: "favorites", label: "Favorite Plants List"},
+          {link: "banned", label: "Banned Plants List"}
+        ]} fixed_width={true}>
+          <Col id="favorites">
             {(this.state.favorites.length > 0) ? this.state.favorites.map(plant => <PlantCard
-              key={plant.id}
-              size="s12"
-              id={plant.id}
-              common_name={plant.common_name}
-              scientific_name={plant.scientific_name}
-              image={plant.image}
-              shade_tolerance={plant.shade_tolerance}
-              favorite={true}
-              userFav={true}
-              handleDeleteEvent={() => this.unfavoritePlant(plant.id)} />) : <p className="center">You currently have no favorite plants.</p>}
+                key={plant.id}
+                size="s12 m4"
+                id={plant.id}
+                common_name={plant.common_name}
+                scientific_name={plant.scientific_name}
+                image={plant.image}
+                shade_tolerance={plant.shade_tolerance}
+                favorite={true}
+                userFav={true}
+                handleDeleteEvent={() => this.unfavoritePlant(plant.id)} />) : <p className="center">You currently have no favorite plants.</p>}
           </Col>
 
-          <Col size="s12 m4">
-            <h3 className="center">Preferred Environment</h3>
-
-            <p><strong>Primary Plant Locations: </strong>{this.state.plant_location.join(", ")}</p>
-
-            <p><strong>Average Shade Tolerance: </strong>{this.state.plant_light}</p>
-
-            <p><strong>Average Watering Frequency: </strong>{this.state.plant_water}</p>
-
-            <p><strong>Pets to Consider: </strong>{this.state.plant_pets.join(", ")}</p>
-
-            <p><strong>Allergies?: {this.state.plant_allergy ? "Yes" : "No"}</strong></p>
-          </Col>
-
-          <Col size="s12 m4">
-            <h3 className="center">Banned Plants</h3>
+          <Col id="banned">
             {(this.state.banned.length > 0) ? this.state.banned.map(plant => <PlantCard
-              key={plant.id}
-              size="s12"
-              id={plant.id}
-              common_name={plant.common_name}
-              scientific_name={plant.scientific_name}
-              image={plant.image}
-              shade_tolerance={plant.shade_tolerance}
-              banned={true}
-              userBan={true}
-              handleUnBanEvent={() => this.unBanPlant(plant.id)} />) : <p className="center">You currently have no banned plants.</p>}
+                key={plant.id}
+                size="s12 m4"
+                id={plant.id}
+                common_name={plant.common_name}
+                scientific_name={plant.scientific_name}
+                image={plant.image}
+                shade_tolerance={plant.shade_tolerance}
+                banned={true}
+                userBan={true}
+                handleUnBanEvent={() => this.unBanPlant(plant.id)} />) : <p className="center">You currently have no banned plants.</p>}
           </Col>
-        </Row>
+        </Tabs>
       </Container>
     )
   };
