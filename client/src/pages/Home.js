4cb33c777c262dccad79212a35cd82ab824c6e29
Danny <jacquez.j.daniel@gmail.com>
2019-03-28 22:14:52 -0700

    home css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -32,7 +32,7 @@ class Home extends Component {
               <SliderItem image="images/flower2.jpeg" caption="Looking for the right plant?">
                 <Link to="/search"><h3>Search Plants</h3></Link>
               </SliderItem>
-              <SliderItem image={(this.state.random_plant.image) ? this.state.random_plant.image : "images/flower3.jpeg"} caption={((this.state.random_plant.common_name) && ("Commonly known as " + this.state.random_plant.common_name + ",")) + " is your random plant."}>
+              <SliderItem image={(this.state.random_plant.image) ? this.state.random_plant.image : "images/flower4.jpeg"} caption={((this.state.random_plant.common_name) && ("Commonly known as " + this.state.random_plant.common_name + ",")) + " is your random plant."}>
                 <h3 className="left-align">{this.state.random_plant.scientific_name}</h3>
               </SliderItem>
             </Slider>
@@ -40,15 +40,15 @@ class Home extends Component {
         </Row>
 
         <Row id="planter-box">
-          <Col size="s12 m4 center plant-box">
-            <h2>
+          <Col size="s12 plant-box">
+            <h2 className="center-align">
               <a href="/questionnaire" className="teal-text text-darken-2">Plant Matcher</a>
             </h2>
 
-            <p>Don't know where to start? Take our quiz to find the plant that is right for you!</p>
+            <p className="center-align">Don't know where to start? Take our quiz to find the plant that is right for you!</p>
           </Col>
 
-          <Col size="s12 m4 center plant-box">
+          <Col size="s6 plant-box">
             <h2>
               <a href="/search" className="teal-text text-darken-2">Search Plants</a>
             </h2>
@@ -56,12 +56,12 @@ class Home extends Component {
             <p>Have an idea of the plant you want? Use our search engine to find the plant you're looking for.</p>
           </Col>
 
-          <Col size="s12 m4 center plant-box">
-            <h2>
+          <Col size="s6 plant-box">
+            <h2 className="right-align">
               <a href="/user" className="teal-text text-darken-2">User Planter</a>
             </h2>
 
-            <p>Already a member of Greenthumb Therapy? Manage your plants, whether you love them or can't stand them.</p>
+            <p className="right-align">Already a member of Greenthumb Therapy? Manage your plants, whether you love them or can't stand them.</p>
           </Col>
         </Row>
       </Container>
